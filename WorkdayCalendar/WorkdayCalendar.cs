ad0b43961b012656f2c94d0201fa353a98f90f0a
Kacper Kedzierski <46610139+Nsquik@users.noreply.github.com>
2022-02-03 09:29:57 +0100

    Add IWorkdayCalendar interface + holidays handling init

---
+++ WorkdayCalendar/WorkdayCalendar.cs
@@ -0,0 +1,37 @@
+﻿using WorkdayCalendar.Interfaces;
+
+namespace WorkdayCalendar;
+
+public class RecurringHoliday
+{
+    public int Month;
+    public int Day;
+}
+
+
+public class WorkdayCalendar : IWorkdayCalendar
+{
+    private readonly List<DateTime> _holidays = new List<DateTime>();
+    private readonly List<RecurringHoliday> _recurringHolidays = new List<RecurringHoliday>();
+    
+    
+    public void SetHoliday(DateTime date)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SetRecurringHoliday(int month, int day)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SetWorkdayStartAndStop(int startHours, int startMinutes, int stopHours, int stopMinutes)
+    {
+        throw new NotImplementedException();
+    }
+
+    public DateTime GetWorkdayIncrement(DateTime startDate, decimal incrementInWorkdays)
+    {
+        throw new NotImplementedException();
+    }
+}
