1341c114af1e37057b51ace37c7666a81883f3ba
Kacper Kedzierski <46610139+Nsquik@users.noreply.github.com>
2022-02-03 09:44:25 +0100

    Add WorkdayHours class

---
+++ WorkdayCalendar/WorkdayHours.cs
@@ -0,0 +1,20 @@
+﻿namespace WorkdayCalendar;
+
+public class WorkdayHours
+{
+    public int StartHour;
+    public int EndHour;
+
+    public int StartMinute;
+    public int EndMinute;
+
+    
+    public WorkdayHours(int startHour, int endHour, int startMinute, int endMinute)
+    {
+        StartHour = startHour;
+        EndHour = endHour;
+        StartMinute = startMinute;
+        EndMinute = endMinute;
+    }
+
+}
