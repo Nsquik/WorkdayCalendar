d75769aef6593180f4f5d8f9c233c22f75a69fac
Kacper Kedzierski <46610139+Nsquik@users.noreply.github.com>
2022-02-03 09:16:29 +0100

    init project

---
+++ WorkdayCalendar.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace WorkdayCalendar.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
