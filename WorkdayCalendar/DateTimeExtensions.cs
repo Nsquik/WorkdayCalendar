ed8aaf9532723da949d14b7ce0b078a8c7c3a552
Kacper Kedzierski <46610139+Nsquik@users.noreply.github.com>
2022-02-03 11:07:39 +0100

    Add DateTime extensions to check if day is a valid workday.

---
+++ WorkdayCalendar/DateTimeExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace WorkdayCalendar;
+
+public static class DateTimeExtensions
+{
+    public static bool IsWeekend(this DateTime date)
+    {
+        return date.DayOfWeek is DayOfWeek.Saturday or DayOfWeek.Sunday;
+    }
+
+    private static bool IsValidWorkday(this DateTime date, WorkdayCalendar calendar )
+    {
+        if (calendar.GetRecurringHolidays().Any(rh => rh.Day == date.Day && rh.Month == date.Month))
+        {
+            return false;
+        }
+
+        if (date.IsWeekend())
+            return false;
+
+        return !calendar.GetHolidays().Any(h => h.Month == date.Month && h.Day == date.Day && h.Year == date.Year);
+    }
+}
